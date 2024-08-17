97bb1b323da86978b2e928e679a3f3c2c13f76d4
Yanita <soloangelle@gmail.com>
2024-08-17 10:08:37 +0200

    Se agrega libreria para control de limit de peticiones

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { rateLimit } = require ('express-rate-limit');
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    limit: 25,
+    message: {
+        ok: false,
+        message: "Ha excedido el numero de peticiones"
+    }
+})
+
+app.use(limiter)
 
 const api_routes = require('./routes/index');
+const { message } = require('./validation/product.validation');
 // Share public folder
 app.use(express.static('public'));
 // Middelewares
